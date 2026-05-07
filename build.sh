450dbc8c508e558572db7a5431291952a1081462
JW-Albert <ru04jo30801@gmail.com>
2026-05-07 16:01:15 +0800

    Update build script to remove existing Python 3.12.13 virtual environment before creating a new one. Clarifies environment activation message for better user understanding.

--- build.sh
+++ build.sh
@@ -4,11 +4,14 @@ set -e
 
 clear
 
-echo "[INFO] Build Virtual Python3 Env."
+echo "[INFO] Remove Virtual Python3.12.13 Environment."
+rm -rf venv
+
+echo "[INFO] Build Virtual Python3.12.13 Environment.."
 python3.12 -m venv venv
 echo "[INFO] Venv Build Completed"
 
-echo "[INFO] Activate the Python3 venv"
+echo "[INFO] Activate the Python3.12.13 Environment."
 source venv/bin/activate
 
 echo "[INFO] Upgrade PIP Version."
